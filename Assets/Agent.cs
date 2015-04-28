6bf22bd654d78ff1bd3bb10135ae722c5bfe6150
saltire sable <saltiresable@gmail.com>
2015-04-27 20:06:50 -0400

    Initial commit of project
    - Edit > Project Settings > Editor: Visible Meta Files
    - Delete/ignore "Library" and "Temp" folders

---
+++ Assets/Agent.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Agent : MonoBehaviour {
+
+	public GameObject target;
+
+	private NavMeshAgent agent;
+
+	void Start ()
+	{
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	void FixedUpdate ()
+	{
+		// agent will move to mouse click
+		// if (Input.GetMouseButtonDown(0))
+		// {
+		// 	Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		//
+		// 	RaycastHit hit;
+		// 	if (Physics.Raycast(ray, out hit))
+		// 	{
+		// 		if (hit.collider.tag == "Ground")
+		// 		{
+		// 			agent.SetDestination(hit.point);
+		// 		}
+		// 	}
+		// }
+
+		// agent will move to target gameobject
+		agent.SetDestination(new Vector3(target.transform.position.x, 0f, target.transform.position.z));
+	}
+}
---
+++ Assets/SimpleMove.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMove : MonoBehaviour {
+
+	private CharacterController player;
+	private float speed = 10f;
+
+	void Start ()
+	{
+		player = GetComponent<CharacterController>();
+	}
+
+	void FixedUpdate ()
+	{
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+
+		Vector3 moveDirection = new Vector3(h * speed, -100f, v * speed);
+		player.Move(moveDirection * Time.deltaTime);
+	}
+}
