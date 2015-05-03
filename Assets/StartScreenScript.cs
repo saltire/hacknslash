93fcc94c468b3c2729b873ed5d339d77f68edc62
Aaron McLeod <sircoolguy99@gmail.com>
2015-05-03 14:52:27 -0400

    added input handling for start

--- Assets/StartScreenScript.cs
+++ Assets/StartScreenScript.cs
@@ -10,7 +10,7 @@ public class StartScreenScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(KeyCode.KeypadEnter) || Input.GetKey(KeyCode.Space)) {
+		if (Input.GetKey(KeyCode.KeypadEnter) || Input.GetKey(KeyCode.Space) || Input.GetAxis("Attack1") > 0 || Input.GetAxis("Attack2") > 0) {
 			GameObject.FindGameObjectWithTag("FadeObject").GetComponent<TransitionScript>().StartFade();
 		}
 	}
