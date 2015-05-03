03bc11b4fff82a4c4ef4f9bf6ae58a765e1461ba
saltire sable <saltiresable@gmail.com>
2015-05-03 17:49:42 -0400

    Victory splash

---
+++ Assets/scripts/WinScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinScript : MonoBehaviour {
+
+	private EnemySpawnPoint[] spawnPoints;
+	private GameObject victory;
+
+	void Start () {
+		spawnPoints = gameObject.GetComponentsInChildren<EnemySpawnPoint> ();
+		victory = gameObject.transform.Find("Victory").gameObject;
+	}
+	
+	void Update () {
+		if (!victory.activeSelf) {
+			foreach (EnemySpawnPoint spawnPoint in spawnPoints) {
+				if (!spawnPoint.ReachedLimit ()) {
+					return;
+				}
+			}
+
+			if (GameObject.FindObjectsOfType<EnemyNavMesh> ().Length == 0) {
+				victory.SetActive (true);
+			}
+		}
+	}
+}
